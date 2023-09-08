037924a99c6f2646cfa2155a98e9663700a24469
UsefulSan <usefulsan@yandex.ru>
2023-09-08 12:26:48 +0300

    Добавлен README.md Отлажен get_new_filename.py

--- get_new_filename.py
+++ get_new_filename.py
@@ -1,22 +1,3 @@
-# import os
-#
-#
-# def get_new_filename(path):
-#     if type(path) == str:
-#         path = os.path.normpath(path)
-#         print(type(path))
-#     if not os.path.exists(path):
-#         return path
-#     else:
-#         numb = 1
-#         while True:
-#             new_path = f'{path.parent}\\{path.stem}_{numb}{path.suffix}'
-#             if not os.path.exists(new_path):
-#                 return new_path
-#             numb += 1
-#
-# get_new_filename('D:/Данные со старого диска/работа/ООО Тепло/Устав/Устав ЮЛ/123.docx')
-
 import os
 from pathlib import Path
 
@@ -33,7 +14,3 @@ def get_new_filename(path):
             if not os.path.exists(new_path):
                 return new_path
             numb += 1
-
-
-new_name = get_new_filename('D:/Данные со старого диска/работа/ООО Тепло/Устав/Устав ЮЛ/123.docx')
-print(new_name)
