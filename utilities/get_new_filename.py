ea56d51858f01cd025d163c3ac66ed353d146ef8
UsefulSan <usefulsan@yandex.ru>
2023-09-11 15:41:37 +0300

    убрал лишние строки помогающие в тесте.

--- utilities/get_new_filename.py
+++ utilities/get_new_filename.py
@@ -11,14 +11,12 @@ def get_new_filename(path: Path | str) -> Path:
     if isinstance(path, str):
         path = Path(path)
     if not os.path.exists(os.path.join(path.parent, path.name)):
-        print(type(path))
         return path
     else:
         numb = 1
         while True:
             new_path = path.parent / f'{path.stem}_{numb}{path.suffix}'
             if not os.path.exists(new_path):
-                print(type(new_path))
                 return new_path
             numb += 1
 
