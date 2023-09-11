0926fbb71eca027f4903758982eec4d8ada6b642
UsefulSan <usefulsan@yandex.ru>
2023-09-11 15:39:56 +0300

    добавил комментарии и типизацию

--- utilities/get_new_filename.py
+++ utilities/get_new_filename.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 
-def get_new_filename(path: str | Path) -> Path:
+def get_new_filename(path: Path | str) -> Path:
     """
     Создает путь и новое не занятое имя для файла
     :param path: путь к файлу в виде строки или path
@@ -11,11 +11,14 @@ def get_new_filename(path: str | Path) -> Path:
     if isinstance(path, str):
         path = Path(path)
     if not os.path.exists(os.path.join(path.parent, path.name)):
+        print(type(path))
         return path
     else:
         numb = 1
         while True:
             new_path = path.parent / f'{path.stem}_{numb}{path.suffix}'
             if not os.path.exists(new_path):
+                print(type(new_path))
                 return new_path
             numb += 1
+
--- utilities/tiff_to_word.py
+++ utilities/tiff_to_word.py
@@ -1,6 +1,7 @@
 """Guide https://github.com/UB-Mannheim/tesseract/wiki"""
 
 import tkinter as tk
+from pathlib import Path
 from tkinter import filedialog
 
 import docx
@@ -10,37 +11,38 @@ from PIL import Image
 from utilities.get_new_filename import get_new_filename
 
 tess_config = '--psm 3'
+# Указывается место установки tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\usefu\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 
-def tiff_to_docx(tiff_file, docx_file):
-    # Открываем TIFF файл с помощью Pillow
+def tiff_to_docx(tiff_file: str, docx_file: Path) -> None:
+    """
+    Открывает tiff файл, распознаем текст на изображении с помощью pytesseract и сохраняет его в docx
+    :param tiff_file: путь к tiff файлу
+    :param docx_file: путь к docx файлу
+    """
     im = Image.open(tiff_file)
-    # Распознаем текст на изображении с помощью pytesseract
     text = pytesseract.image_to_string(im, lang='rus', config=tess_config)
     try:
-        # Открываем существующий Word-документ
         doc = docx.Document(str(docx_file))
     except docx.opc.exceptions.PackageNotFoundError:
-        # Создаем новый Word-документ, если файл не существует
         doc = docx.Document()
     doc.add_paragraph(text)
-    # Сохраняем Word документ
     doc.save(docx_file)
 
 
-def processing_to_folder():
+def processing_to_folder() -> None:
     tiff_files = tiff_entry.get().split(', ')
     docx_file = docx_entry.get()
     directory = ''.join(tiff_files[0]).rsplit('/', 1)[0]
     docx_file = directory + '/' + docx_file + '.docx'
-    docx_file = get_new_filename(docx_file)
+    docx_file_path = get_new_filename(docx_file)
     for file in tiff_files:
         if file.lower().endswith(".tiff"):
-            tiff_to_docx(file, docx_file)
+            tiff_to_docx(file, docx_file_path)
 
 
-def select_tiff_files():
+def select_tiff_files() -> None:
     file_paths = filedialog.askopenfilenames(filetypes=[("TIFF files", ".tiff"), ("All files", ".*")])
     tiff_entry.delete(0, tk.END)
     tiff_entry.insert(0, ', '.join(file_paths))
